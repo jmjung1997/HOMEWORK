1163e9c1dc91d180461d0cdb2469252280e69bfb
jaemin <jmjung1997@gmail.com>
2021-04-21 16:26:16 +0900

    second commit

--- doubly-linked-list.c
+++ doubly-linked-list.c
@@ -120,13 +120,31 @@ int main()
 }
 
 
-int initialize(headNode** h) {
+int initialize(headNode** h)//연결리스트 초기화 함수, headnode의 위치 주소를 불러와 그에 해당하는 값을 수정하기 위해 더블포인터를 사용
+{
+	if (*h != NULL) //h를 가리키는 값 NULL이 아닐 때
+        freeList(*h);//h가 가리키는 헤드노드를 프리 시켜준다
 
+    *h = (headNode*)malloc(sizeof(headNode)); //h가 가리키는 헤드노드에 동적할당을 해준다
+	(*h)->first=NULL;//헤드노드의 first 링크를 NULL 값으로 초기화 한다 
 	return 1;
 }
 
-int freeList(headNode* h){
-	return 0;
+int freeList(headNode* h)
+{
+	 /* h와 연결된 listNode 메모리 해제
+     * headNode도 해제되어야 함.
+     */
+    listNode* p = h->first;
+
+    listNode* prev = NULL;
+    while (p != NULL) {//p가 NULL일 때 까지 반복한다
+        prev = p; //prev에 p를 대입
+        p = p->rlink; //p노드를 한칸 이동한다
+        free(prev);//prev에 들어 있는 node를 메모리 할당해제 시켜준다
+    }
+    free(h);
+    return 0;
 }
 
 
@@ -158,9 +176,30 @@ void printList(headNode* h) {
 /**
  * list에 key에 대한 노드하나를 추가
  */
-int insertLast(headNode* h, int key) {
+int insertLast(headNode* h, int key)
+{
+	listNode* node = (listNode*)malloc(sizeof(listNode)); //노드를 동적할당 해준다.
+    listNode* p = h->first;//연결리스트의 첫번째 노드 주소를 p에 대입
+    node->key = key; //node의 키값 대입
+    node->rlink = NULL;//node의 rlink를 NULL 값으로 비워둔다
+	node->llink = NULL;//node의 llink를 NULL 값으로 비워둔다
+    if(p==NULL)//마지막에 노드 넣으려고 하는데 연결리스트에 아무것도 없을 때
+    {       
+      h->first = node; //h->first를 node 위치에 연결시킨다
+	  node->llink=h->first; //node->llink를 헤드노드에 연결시키다
+	}
+    else //연결리스트에 무엇인가 들어있을 때
+    {
+        while (p->rlink != NULL)//p->rlink를 가리키는 값이 NULL값이 나올 때 까지 반복한다.  
+        {
+            p = p->rlink;
+        }
+        p->rlink = node;//맨마지막에 있던 노드를 새로 추가시켜주는 노드에 연결시켜준다
+		node->llink=p;
+    }
+    return 0;
 
-	return 0;
+	
 }
 
 
@@ -168,7 +207,8 @@ int insertLast(headNode* h, int key) {
 /**
  * list의 마지막 노드 삭제
  */
-int deleteLast(headNode* h) {
+int deleteLast(headNode* h)
+ {
 
 
 	return 0;
@@ -179,8 +219,24 @@ int deleteLast(headNode* h) {
 /**
  * list 처음에 key에 대한 노드하나를 추가
  */
-int insertFirst(headNode* h, int key) {
-	return 0;
+int insertFirst(headNode* h, int key)
+{
+	
+    listNode* node = (listNode*)malloc(sizeof(listNode)); //노드를 동적할당 해준다.
+    listNode* p = h->first;//연결리스트의 첫번째 노드 주소를 p에 대입
+    node->key = key; //node의 키값 대입
+    node->rlink = NULL;//node의 rlink를 NULL 값으로 비워둔다
+	node->llink = NULL;//node의 llink를 NULL 값으로 비워둔다
+    
+	node->rlink=h->first;//node rlink를 기존 p노드에 연결
+    node->llink=h;//node llink를 헤드노드에 연결
+	h->first=node;//h->first는 새로 추가하는 노드에 연결
+	p->llink=node;//기존에 있던 노드 llink를 새로 추가할 노드에 연결 
+
+	h->first = node;//추가할 node를 h->first에 연결한다
+
+    return 0;
+	
 }
 
 /**
