9cbf132362b8210d8cd95445c33ca28d939b96da
jaemin <jmjung1997@gmail.com>
2021-05-02 16:41:08 +0900

    second commit

--- binary-search-tree-1.c
+++ binary-search-tree-1.c
@@ -126,45 +126,59 @@ int initializeBST(Node** h) {
 
 
 
-void inorderTraversal(Node* ptr)
-{	
+void inorderTraversal(Node* ptr)//중위순회 출력하는 함수
+{	/*recursive를 통하여 최대한 왼쪽 노드 끝까지 간다음 왼쪽자식, 부모, 오른쪽자식 순으로 출력한다*/
 	
 	if(ptr!=NULL)
 	{
-		inorderTraversal(ptr->left);
-		printf("[ %d ] ",ptr->key);//키값을 출력한다
-		inorderTraversal(ptr->right);
+		inorderTraversal(ptr->left);//자신의 함수에 ptr->left의 주소를 보내 재귀함수를 호출한다
+		printf("[ %d ] ",ptr->key);//현재노드의 키값을 출력한다
+		inorderTraversal(ptr->right);//자신의 함수에 ptr->right의 주소를 보내 재귀함수를 호출한다
 	}
-	return;
+	
 }
 
-void preorderTraversal(Node* ptr)
-{
+void preorderTraversal(Node* ptr)//전위순회 출력하는 함수
+{/*recursive를 통하여 먼저 부모노드를 출력한 다음 왼쪽 자식노드로 계속가면서 부모노드를 먼저 출력한 다음 마지막으로 오른쪽 자식노드들을 
+위로 올라오면서 출력한다*/
+	if(ptr!=NULL)
+	{
+		printf("[ %d ] ",ptr->key);//현재 노드의 키값을 출력한다
+		preorderTraversal(ptr->left);//자신의 함수에 ptr->left의 주소를 보내 재귀함수를 호출한다
+		preorderTraversal(ptr->right);//자신의 함수에 ptr->right의 주소를 보내 재귀함수를 호출한다
+	}
+}
+
+void postorderTraversal(Node* ptr)//후위순회 출력하는 함수
+{/*resursive를 통하여 가장 밑에 있는 왼쪽 자식노드 까지 도달한 다음 올라오면서 오른쪽 자식, 부모노드 순으로 출력한다*/
+	if(ptr!=NULL)
+	{
+		postorderTraversal(ptr->left);//자신의 함수에 ptr->left의 주소를 보내 재귀함수를 호출한다
+		postorderTraversal(ptr->right);//자신의 함수에 ptr->right의 주소를 보내 재귀함수를 호출한다
+		printf("[ %d ] ",ptr->key);//현재 노드의 키값을 출력한다
+	}
 
 }
 
-void postorderTraversal(Node* ptr)
-{
 
-}
-
-
-int insert(Node* head, int key)
+int insert(Node* head, int key)//트리에 데이터를 삽입하는 함수
 {
 	
 	Node* node = (Node*)malloc(sizeof(Node)); //노드를 동적할당 해준다.
 	Node* p = head->left;//연결리스트의 첫번째 노드 주소를 p에 대입
-    node->key = key; //node의 키값 대입
+    Node* parent=NULL;//부모노드를  포인터 변수로 선언한다
+	node->key = key; //node의 키값 대입
     node->right = NULL;//node의 right를 NULL 값으로 초기화한다
 	node->left = NULL;//node의 left를 NULL 값으로 초기화한다
 	if(p==NULL) //만약 p가 비어있을때
 	{
-		 head->left=node;//p에 node 주소를 대입하여 루트가 된다
+		 head->left=node;//head->left를 가리키는 값을 node 주소를 대입하여 루트가 된다
 	}
 	else
 	{
-		while(p->left!=NULL||p->right!=NULL)//p의 left가 비어있을 때 까지 반복
+		while(p!=NULL)//p가 비어있을 때 까지 반복
 		{
+			parent=p;//현재 p를 부모노드로 대입한다
 			if(node->key<p->key) //node->key가 p->key보다 작을 때
 			{
 				p=p->left; // p노드 왼쪽으로 이동한다
@@ -174,13 +188,13 @@ int insert(Node* head, int key)
 				p=p->right;// p노드 오른쪽으로 이동한다
 			}
 		}
-		if(node->key<p->key) 
+		if(node->key<parent->key)//부모노드보다 넣으려는 값이 작을경우
 		{
-			p->left=node;
+			parent->left=node;
 		}
-		else
+		else//부모노드보다 넣으려는 값이 클 경우
 		{
-			p->right=node;
+			parent->right=node;
 		}
 	}
 	return 0;
@@ -191,14 +205,45 @@ int deleteLeafNode(Node* head, int key)
 
 }
 
-Node* searchRecursive(Node* ptr, int key)
+Node* searchRecursive(Node* ptr, int key)//recursive를 통하여 해당하는 노드 주소 찾는 함수
 {
-
+	if (!ptr) //해당하는 노드가 없을 때 NULL값을 반환한다
+	{
+	return NULL;
+	}
+	else if (key == ptr->key)//해당하는 key를 가지고 있는 노드를 발견할 때 해당 노드 주소를 반환한다
+	{
+	 return ptr;
+	}
+	else if (key < ptr->key)//key값이 ptr노드가 가지고 있는 키값 보다 작을때
+	{
+		return searchRecursive(ptr->left, key);//재귀함수를 ptr->left주소로 호출
+	}
+	else//key값이 ptr노드가 가지고 있는 키값 보다 클 때
+	{
+		return searchRecursive(ptr->right, key);//재귀함수를 ptr->right주소로 호출
+	}
 }
 
-Node* searchIterative(Node* head, int key)
-{
-
+Node* searchIterative(Node* head, int key)//iterative를 통하여 해당하는 노드 주소 찾는 함수
+{	
+	Node *ptr=head->left;//head->left를 ptr노드에 대입한다
+	while (ptr!=NULL)//ptr이 NULL이 나올 때 까지 반복한다
+	{
+		if (key == ptr->key)// key 값과 해당하는 노드의 key 값이 일치할 때
+		{
+		 	return ptr; //해당하는 노드의 주소를 반환한다
+		}
+		else if (key < ptr->key)// key 값이 노드가 가지고 있는 key값 보다 작을 때
+		{
+			ptr = ptr->left; //ptr의 노드를 ptr의 왼쪽 자식 노드로 옮긴다
+		}
+		else// key 값이 노드가 가지고 있는 key값 보다 클 때
+		{
+			ptr = ptr->right;//ptr의 노드를 ptr의 오른쪽 자식 노드로 옮긴다
+		}
+	}
+		return NULL;//NULL 값을 반환한다
 }
 
 
